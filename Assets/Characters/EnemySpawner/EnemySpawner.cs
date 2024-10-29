f1c9df83af4b323cbae31b9a1b3316532dc30f29
Quincy Kapsner <therealquinner@gmail.com>
2024-10-28 17:29:24 -0700

    Added EnemySpawner and WaveController
    EnemySpawner and WaverController work, just need to be fine tuned for the whole length of the game.
    Need to add more in-between waves. Maybe add repeatable final wave or just time out how long charge takes and base waves off that.

--- Assets/Characters/EnemySpawner/EnemySpawner.cs
+++ Assets/Characters/EnemySpawner/EnemySpawner.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject enemyPrefab;
+    public GameObject orc1;
+    public GameObject orc2;
+    public GameObject orc3;
+    public float spawnRate = 1f;
 
-    private float startSpawnTime;
-    private float endSpawnTime;
-    private float spawnInterval = 1.0f;
-
-    void FixedUpdate()
+    public void SpawnWave(int wave, System.Action onWaveComplete)
     {
-        
+        StartCoroutine(SpawnEnemies(wave, onWaveComplete));
     }
+
+    IEnumerator SpawnEnemies(int wave, System.Action onWaveComplete)
+    {
+        switch (wave) {
+            case 1:
+                Instantiate(orc1, transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(orc1, transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(spawnRate);
+                Instantiate(orc1, transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(orc2, transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(spawnRate);
+                Instantiate(orc1, transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(orc2, transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(spawnRate);
+                Instantiate(orc1, transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(spawnRate);
+                Instantiate(orc1, transform.position, Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(orc1, transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(spawnRate);
+                Instantiate(orc2, transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(spawnRate);
+                Instantiate(orc3, transform.position, Quaternion.identity);
+                break;
+        }
+        yield return new WaitForSeconds(1f);
+        onWaveComplete.Invoke();
+    }
+
 }
--- Assets/Characters/EnemySpawner/WaveController.cs
+++ Assets/Characters/EnemySpawner/WaveController.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveController : MonoBehaviour
 {
+    public EnemySpawner[] spawners;
+    public int currentWave = 1;
+    private bool waveInProgress = false;
+    public float waveDelay = 30f; // wait time between waves
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(StartWaves());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator StartWaves()
     {
-        
+        while (currentWave <= 5) // hardcoded for 5 waves
+        {
+            waveInProgress = true;
+            SpawnWave(currentWave);
+            yield return new WaitUntil(() => !waveInProgress);
+            yield return new WaitForSeconds(waveDelay); // wait time between waves
+            currentWave++;
+        }
+        Debug.Log("all waves completed"); // all waves complete
+    }
+
+    void SpawnWave(int wave)
+    {
+        foreach (var spawner in spawners)
+        {
+            spawner.SpawnWave(wave, () => waveInProgress = false);
+        }
     }
 }
